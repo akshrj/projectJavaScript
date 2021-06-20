50d19d2ed5304ce7d3074f13e159b4e3be1c1add
akshrj <akshrj4@gmail.com>
2021-06-20 23:55:21 +0530

    Create twiceAndHalf.js
    print another list such that every element of array being twice and half of elements in the current list

---
+++ module1/twiceAndHalf.js
@@ -0,0 +1,21 @@
+let arr = [0, 2, 4, 6, 8, 10, 12, 14, 16];
+
+let twiceArr = [];
+let halfArr = [];
+
+function twice(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		twiceArr.push(arr[i] * 2);
+	}
+	return twiceArr;
+}
+
+function half(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		twiceArr.push(arr[i] / 2);
+	}
+	return halfArr;
+}
+
+console.log(twice(arr));
+console.log(half(arr));
